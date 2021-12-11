2b8f6c7de90a4cc8302a5773b3371dccb8d524ff
Oliver Tacke <o.tacke@posteo.de>
2021-12-11 14:26:06 +0100

    Add initial version

---
+++ src/entries/h5peditor-image-pairing-behaviour.js
@@ -0,0 +1,6 @@
+import "../styles/h5peditor-image-pairing-behaviour.css";
+import ImagePairBehaviour from "../scripts/h5peditor-image-pairing-behaviour";
+
+// Load library
+H5PEditor = H5PEditor || {};
+H5PEditor.widgets.imagepairbehaviour = ImagePairBehaviour;
